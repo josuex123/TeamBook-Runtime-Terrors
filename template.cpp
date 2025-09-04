f1243c1608022111e323056ae3bb5c41635581cc
Josuex123 <josue7pedro@gmail.com>
2025-09-03 22:36:23 -0400

    Template.cpp

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define int long long 
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define endl "\n"
+#define F first
+#define S second
+#define vi vector<int>
+#define ii pair<int,int>
+#define debug cout<<"entro"<<endl;
+#define debug2(x) cout<<":"<<x<<endl;
+#define vii vector<ii>
+#define all(x) x.begin(),x.end()
+#define double long double
+#define forn(i,n) for(int i = 0; i < n; i++)
+#define fore(i, b, e) for(int i = b; i < e; i++)
+#define foree(i, b, e) for(int i = b; i <= e; i++)
+#define forg(i, b, e, m) for(int i = b; i < e; i+=m)
+using namespace std;
+const int INF = 1e9 + 7;
+const int tam = 1e6 + 5;
+int dp[tam];
+using ll = long long;
+
+
+
+void solve(){
+  
+}
+
+signed main(){
+   FIO
+   // USACO
+   // ifstream fin("reststops.in");
+   // ofstream fout("reststops.out");
+
+   // UVA
+   // freopen("censor.in", "r", stdin);
+    // freopen("censor.out", "w", stdout);
+
+   // int t;
+   // cin >> t;
+   // while(t--)
+   solve();
+    
+}
