a013547c7f8b30f5d5b0426d33501a066b3953e0
Josuex123 <josue7pedro@gmail.com>
2025-09-12 19:28:33 -0400

    Bipartido add

--- Grafos/BFS.cpp
+++ Grafos/BFS.cpp
@@ -15,3 +15,26 @@ vector<int> bfs(vector<vector<int>>& g , int u) {
     }
     return dis;
 }
+bool esBipartito(vector<vector<int>>& grafo, int n) {
+    vector<int> color(n, 0); // 0 = no coloreado, 1 y 2 = colores
+    for (int i = 0; i < n; ++i) {
+        if (color[i] == 0) {
+            queue<int> q;
+            q.push(i);
+            color[i] = 1;
+
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                for (int v : grafo[u]) {
+                    if (color[v] == 0) {
+                        color[v] = 3 - color[u]; // alterna entre 1 y 2
+                        q.push(v);
+                    } else if (color[v] == color[u]) {
+                        return false; // hay conflicto de colores
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
