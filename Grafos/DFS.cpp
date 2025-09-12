ba78f9670373e43af9c2dd0fe5734eb649c7585d
Josuex123 <josue7pedro@gmail.com>
2025-09-12 19:29:29 -0400

    Transp add

--- Grafos/DFS.cpp
+++ Grafos/DFS.cpp
@@ -6,3 +6,25 @@ void dfs(int v, vector<bool>& visited, const vector<vector<int>>& graph) {
         }
     }
 }
+bool dfsCiclo(int v,int parent, vector<bool>& visi, vector<vector<int>>& gr){
+    visi[v] = true;
+    for(int vis : gr[v]){ 
+        if(!visi[vis]){
+            if(dfsCiclo(vis, v,visi, gr))
+                return true;
+        }else if(vis != parent){
+            return true;
+        }
+    }
+    return false;
+}
+void tra(int n,vector<vii> G){
+vector<vector<pair<ll,ll>>> Trans(n + 1);
+    foree(i,1,n){
+        for(auto g : G[i]){
+            ll v = g.F;
+            ll cost = g.S;
+            Trans[v].pb({i,cost});
+        }
+    }
+}
