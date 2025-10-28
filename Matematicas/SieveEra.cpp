0a4fe450ae7b3007c195f508a8e0e4aacbeb42f4
Josuex123 <josue7pedro@gmail.com>
2025-10-27 22:56:43 -0400

    add: smaller prime factor

--- Matematicas/SieveEra.cpp
+++ Matematicas/SieveEra.cpp
@@ -10,3 +10,26 @@ vector<bool> sieve(int n){
     return primes;
 
 }
+vector<bool> sieve(int n) { 
+    vector<bool> spf(n+1);
+    for (int i = 1; i <= n; ++i) {  spf[i] = i;  }
+    for (int i = 2; i * i <= n; ++i) {
+        if (spf[i] == i) { 
+            for (int j = i * i; j <= n; j += i) {
+                if (spf[j] == j) { 
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+    return spf;
+}
+vector<bool>facPrime(int n, vi& primes){
+    vector<bool> fac;
+    while(n > 1){
+        int p = primes[n];
+        fac.pb(p);
+        while(n % p == 0) n /= p;
+    }
+    return fac;
+}
