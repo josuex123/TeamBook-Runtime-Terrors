2b79436e336fe33f391827ccedc7f34c70c4db67
Josuex123 <josue7pedro@gmail.com>
2025-11-05 10:13:50 -0400

    add:number divs

--- Matematicas/SieveEra.cpp
+++ Matematicas/SieveEra.cpp
@@ -10,13 +10,15 @@ vector<bool> sieve(int n){
     return primes;
 
 }
-vector<bool> sieve(int n) { 
-    vector<bool> spf(n+1);
-    for (int i = 1; i <= n; ++i) {  spf[i] = i;  }
+vector<int> sieve(int n) { 
+    vector<int> spf(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        spf[i] = i;
+    }
     for (int i = 2; i * i <= n; ++i) {
-        if (spf[i] == i) { 
+        if (spf[i] == i) {
             for (int j = i * i; j <= n; j += i) {
-                if (spf[j] == j) { 
+                if (spf[j] == j) {
                     spf[j] = i;
                 }
             }
@@ -24,12 +26,25 @@ vector<bool> sieve(int n) {
     }
     return spf;
 }
-vector<bool>facPrime(int n, vi& primes){
-    vector<bool> fac;
-    while(n > 1){
-        int p = primes[n];
-        fac.pb(p);
-        while(n % p == 0) n /= p;
+
+vector<int> facPrime(int n, vector<int>& spf) {
+    vector<int> fac;
+    while (n > 1) {
+        int p = spf[n];
+        fac.push_back(p);
+        while (n % p == 0)
+            n /= p;
     }
     return fac;
+}
+vi numDivs(int n) {
+    vi divs;
+    for(int i = 1; i * i <= n; i++) {
+        if(n % i == 0) {
+            divs.pb(i);
+            if(i != n / i) divs.pb(n / i);
+        }
+    }
+    sort(all(divs));
+    return divs;
 }
