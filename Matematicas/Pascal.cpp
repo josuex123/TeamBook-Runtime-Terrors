0098114509c98d52e81552519fc82493ad338619
Kevin Huayllas <94870936+KevinOmega@users.noreply.github.com>
2025-11-05 16:29:23 -0400

    Implement binomial coefficient function in Pascal.cpp

---
+++ Matematicas/Pascal.cpp
@@ -0,0 +1,13 @@
+int binomialCoefficient(int n , int k){
+    vector<vector<int>> table(n + 1, vector<int>(k+1,0));
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= min(i,k); j++){
+            if(j==0 || j == i){
+                table[i][j] = 1;
+            }else{
+                table[i][j] = table[i-1][j-1] + table[i-1][j];
+            }
+        }
+    }
+    return table[n][k];
+}
