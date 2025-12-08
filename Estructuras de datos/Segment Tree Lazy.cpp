2949c0c948ca264a764d6501d249c9f1080bbb0a
Josuex123 <josue7pedro@gmail.com>
2025-12-08 12:06:28 -0400

    fix: segment tree with lazy

--- Estructuras de datos/Segment Tree Lazy.cpp
+++ Estructuras de datos/Segment Tree Lazy.cpp
@@ -39,25 +39,23 @@ void update(int i, int tl, int tr, int l, int r, int val){
         push(i, tl, tr); 
         return;
     }
-    int tm = (tl + tr) / 2;
-    if(idx <= tm){
-        update(2*i+1, tl, tm, idx, val);
-    }else{
-        update(2*i+2,tm+1,tr,idx,val);
-    }
+   int tm = (tl + tr) / 2;
+   update(2*i+1, tl, tm, l, min(r,tm), val);
+   update(2*i+2,tm+1,tr,max(l,tm+1),r,val);
+    
     T[i] = min(T[2*i+1], T[2*i+2]);
 }
 int query(int i, int tl, int tr, int l, int r){
-    if(l <= tl && tr <= r){
+    push(i, tl, tr);
+    if(l > r) return INF;
+
+    if(tl == l && tr == r){
         return T[i];
-    }
-    if(r < tl || tr < l){
-        return 1e9;
     }
     int tm = (tl + tr) / 2;
 
-    int p1 = query(2*i+1, tl, tm, l, r);
-    int p2 = query(2*i+2, tm+1, tr, l, r);
+    int p1 = query(2*i+1, tl, tm, l, min(r,tm));
+    int p2 = query(2*i+2, tm+1, tr, max(l,tm+1), r);
     
     return min(p1,p2);
 }
